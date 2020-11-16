89e0f2c1e3ced7942b8386055c2deef1e144a2c8
norman4321 <72541828+norman4321@users.noreply.github.com>
2020-11-16 10:08:36 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,14 @@ const router = Router();
 
 router.get('/signup', controller.signup_get);
 router.post('/signup', controller.signup_post);
-router.get('/login', controller.login_get );
+router.get('/login', controller.login_get);
 router.post('/login', controller.login_post);
 router.get('/logout', controller.logout_get);
 router.get('/', controller.index_get);
 router.get('/bookmarks', controller.bookmarks_get);
 router.get('/owned', controller.owned_get);
 router.get('/forgotpass', controller.forgotpass_get);
+router.post('/forgotpass', controller.forgotpass_post);
 router.get('/forgotpasscode', controller.forgotpasscode_get);
 router.get('/enterpassword', controller.enterpassword_get);
 
