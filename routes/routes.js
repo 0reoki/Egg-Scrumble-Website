ba64b908bef3e8985c759177a2d51654a5ca60b8
norman4321 <72541828+norman4321@users.noreply.github.com>
2020-12-29 23:40:08 +0800

    Update routes.js
    Added routes needed for forgot password

--- routes/routes.js
+++ routes/routes.js
@@ -4,15 +4,18 @@ const router = Router();
 
 router.get('/signup', controller.signup_get);
 router.post('/signup', controller.signup_post);
-router.get('/login', controller.login_get );
+router.get('/login', controller.login_get);
 router.post('/login', controller.login_post);
 router.get('/logout', controller.logout_get);
 router.get('/', controller.index_get);
 router.get('/bookmarks', controller.bookmarks_get);
 router.get('/owned', controller.owned_get);
 router.get('/forgotpass', controller.forgotpass_get);
+router.post('/forgotpass', controller.forgotpass_post);
 router.get('/forgotpasscode', controller.forgotpasscode_get);
+router.post('/forgotpasscode', controller.forgotpasscode_post);
 router.get('/enterpassword', controller.enterpassword_get);
+router.post('/enterpassword', controller.enterpassword_post);
 router.get('/cart', controller.cart_get);
 
 module.exports = router;
