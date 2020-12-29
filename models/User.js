0fa82d993310d0734a89aeadb7acc8368ed35c62
norman4321 <72541828+norman4321@users.noreply.github.com>
2020-12-29 23:42:09 +0800

    Update User.js
    Changed User Schema for forgot password

--- models/User.js
+++ models/User.js
@@ -15,32 +15,38 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter an password'],
         minlength: [6, 'Minimum password length is 6 characters'],
     },
-    first_name:{
+    first_name: {
         type: String,
         required: [true, 'Please enter yout first name']
     },
     last_name: {
         type: String,
         required: [true, 'Please enter yout last name'],
+    },
+    code: {
+        type: String,
+        required: [false, 'Password Reset']
     }
 });
 
 // fire a function before doc saved to db
-userSchema.pre('save', async function (next){
+userSchema.pre('save', async function(next) {
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
 })
 
 // static method to login user
-userSchema.statics.login = async function(email, password){
+userSchema.statics.login = async function(email, password) {
     const user = await this.findOne({ email });
-    if(user){
+    if (user) {
         const auth = await bcrypt.compare(password, user.password);
-        if(auth) {
+        if (auth) {
             return user;
-        } throw Error('incorrect password');
-    } throw Error('incorrect email');
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
 }
 
 const User = mongoose.model('user', userSchema);
