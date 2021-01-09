82cbcddb1f1773d379c8186c81775c70f3cd8caa
nakataken <ken.nakata130@gmail.com>
2021-01-09 21:42:35 +0800

    fixed ejs

--- routes/routes.js
+++ routes/routes.js
@@ -19,8 +19,8 @@ router.post('/forgotpasscode', controller.forgotpasscode_post);
 router.get('/enterpassword', controller.enterpassword_get);
 router.post('/enterpassword', controller.enterpassword_post);
 router.get('/cart', controller.cart_get);
+router.get('/book', controller.book_get);
 router.post('/search', search_controller.search_post);
 router.post('/viewbooks', search_controller.viewbooks_post);
-router.get('/book', controller.book_get);
 
 module.exports = router;
